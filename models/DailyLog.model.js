68c25c066498f95a498fb7c82c9b617131bf56a6
marcela rocha <marcelarochamartins@gmail.com>
2023-07-20 22:53:08 +0200

    manipulation of OpenAI data

--- models/DailyLog.model.js
+++ models/DailyLog.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const dailyLogSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: Date, required: true },
   title: { type: String },
   entry: { type: String, required: true },
--- models/Task.model.js
+++ models/Task.model.js
@@ -4,9 +4,12 @@ const { Schema, model } = mongoose;
 const taskSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
+  frequency: { type: String},
   project: { type: Schema.Types.ObjectId, ref: "Project", required: true },
   status: { type: String, enum: ["pending", "doing", "done"], default: "pending" },
   deadline: { type: Date },
+  parentTask: { type: Schema.Types.ObjectId, ref: "Task" }, // Referência à tarefa pai
+  subTasks: [{ type: Schema.Types.ObjectId, ref: "Task" }] // Referências às tarefas filhas (subtasks)
 });
 
 const Task = model("Task", taskSchema);
