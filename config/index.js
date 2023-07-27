fc9d21bf69b0a317b5e5eb7a7fbd2c74c901deb5
marcela rocha <marcelarochamartins@gmail.com>
2023-07-27 16:19:31 +0200

    starting deploy

--- config/index.js
+++ config/index.js
@@ -11,6 +11,8 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 
+const FRONTEND_URL = process.env.ORIGIN;
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -19,7 +21,8 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: "http://localhost:5173"
+      origin: [FRONTEND_URL],
+      credentials: true,
     })
   );
 
