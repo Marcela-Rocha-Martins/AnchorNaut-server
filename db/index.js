96d2d6799945693572f7438bf926b5242289fbd5
marcela rocha <marcelarochamartins@gmail.com>
2023-11-13 19:30:04 +0100

    index and server solved issues

--- db/index.js
+++ db/index.js
@@ -1,12 +1,15 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+require('dotenv').config();
+
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 // const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://finalproject:AiggBeWUlj7bwXgD@finalproject.u2vxvgy.mongodb.net/AnchorNautProject?retryWrites=true&w=majority";
-const MONGO_URI = process.env.MONGODB_URI 
+const MONGO_URI = process.env.MONGODB_URI
+console.log(MONGO_URI, "MONGODB_URI")
 
 mongoose
   .connect(MONGO_URI)
